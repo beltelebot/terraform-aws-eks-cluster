36525a1868cae134bee2fbaaf01746f6e5de97d6
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-02-11 12:31:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,96 +1,30 @@
-locals {
-  enabled = module.this.enabled
-
-  cluster_encryption_config = {
-    resources        = var.cluster_encryption_config_resources
-    provider_key_arn = local.enabled && var.cluster_encryption_config_enabled && var.cluster_encryption_config_kms_key_id == "" ? join("", aws_kms_key.cluster.*.arn) : var.cluster_encryption_config_kms_key_id
-  }
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
 }
 
-module "label" {
-  source  = "cloudposse/label/null"
-  version = "0.24.1"
-
-  attributes = ["cluster"]
-
-  context = module.this.context
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
 }
 
-data "aws_partition" "current" {
-  count = local.enabled ? 1 : 0
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
 }
 
-resource "aws_cloudwatch_log_group" "default" {
-  count             = local.enabled && length(var.enabled_cluster_log_types) > 0 ? 1 : 0
-  name              = "/aws/eks/${module.label.id}/cluster"
-  retention_in_days = var.cluster_log_retention_period
-  tags              = module.label.tags
-}
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "computool-eks"
+  cluster_version = "1.17"
+  subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  vpc_id          = "vpc-1234556abcdef"
 
-resource "aws_kms_key" "cluster" {
-  count                   = local.enabled && var.cluster_encryption_config_enabled && var.cluster_encryption_config_kms_key_id == "" ? 1 : 0
-  description             = "EKS Cluster ${module.label.id} Encryption Config KMS Key"
-  enable_key_rotation     = var.cluster_encryption_config_kms_key_enable_key_rotation
-  deletion_window_in_days = var.cluster_encryption_config_kms_key_deletion_window_in_days
-  policy                  = var.cluster_encryption_config_kms_key_policy
-  tags                    = module.label.tags
-}
-
-resource "aws_kms_alias" "cluster" {
-  count         = local.enabled && var.cluster_encryption_config_enabled && var.cluster_encryption_config_kms_key_id == "" ? 1 : 0
-  name          = format("alias/%v", module.label.id)
-  target_key_id = join("", aws_kms_key.cluster.*.key_id)
-}
-
-resource "aws_eks_cluster" "default" {
-  count                     = local.enabled ? 1 : 0
-  name                      = module.label.id
-  tags                      = module.label.tags
-  role_arn                  = join("", aws_iam_role.default.*.arn)
-  version                   = var.kubernetes_version
-  enabled_cluster_log_types = var.enabled_cluster_log_types
-
-  dynamic "encryption_config" {
-    for_each = var.cluster_encryption_config_enabled ? [local.cluster_encryption_config] : []
-    content {
-      resources = lookup(encryption_config.value, "resources")
-      provider {
-        key_arn = lookup(encryption_config.value, "provider_key_arn")
-      }
+  worker_groups = [
+    {
+      instance_type = "t2.micro"
+      asg_max_size  = 2
     }
-  }
-
-  vpc_config {
-    security_group_ids      = [join("", aws_security_group.default.*.id)]
-    subnet_ids              = var.subnet_ids
-    endpoint_private_access = var.endpoint_private_access
-    endpoint_public_access  = var.endpoint_public_access
-    public_access_cidrs     = var.public_access_cidrs
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.amazon_eks_cluster_policy,
-    aws_iam_role_policy_attachment.amazon_eks_service_policy,
-    aws_cloudwatch_log_group.default
   ]
-}
-
-# Enabling IAM Roles for Service Accounts in Kubernetes cluster
-#
-# From official docs:
-# The IAM roles for service accounts feature is available on new Amazon EKS Kubernetes version 1.14 clusters,
-# and clusters that were updated to versions 1.14 or 1.13 on or after September 3rd, 2019.
-#
-# https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
-# https://medium.com/@marcincuber/amazon-eks-with-oidc-provider-iam-roles-for-kubernetes-services-accounts-59015d15cb0c
-#
-resource "aws_iam_openid_connect_provider" "default" {
-  count = (local.enabled && var.oidc_provider_enabled) ? 1 : 0
-  url   = join("", aws_eks_cluster.default.*.identity.0.oidc.0.issuer)
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  # it's thumbprint won't change for many years
-  # https://github.com/terraform-providers/terraform-provider-aws/issues/10104
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
 }
